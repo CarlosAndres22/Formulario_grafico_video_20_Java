b62502455c4cc2405f3873a1930553c014e8760f
andres poveda <tizam15@hotmail.com>
2017-02-24 19:32:18 -0500

    nuevamente commit

---
+++ src/formulario_grafico_videos_20/Formulario_grafico_videos_20.java
@@ -0,0 +1,17 @@
+
+package formulario_grafico_videos_20;
+
+/**
+ *
+ * @author C.andres
+ */
+public class Formulario_grafico_videos_20 {
+
+    
+    public static void main(String[] args) {
+       Formulario_1 form=new Formulario_1();
+       form.setVisible(true);
+       
+    }
+    
+}
